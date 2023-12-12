fbcb31e65ce804cf5d83c7a6c31e7f3471cb7e60
ko02 <kd1295315@st.kobedenshi.ac.jp>
2023-12-12 13:29:05 +0900

    登録画面

--- resources/views/layouts/header-pc.blade.php
+++ resources/views/layouts/header-pc.blade.php
@@ -1,56 +1,57 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <script src="https://yu.academic-gihara0655.com/bootstrap-5.3.2-dist/js/bootstrap.min.js"></script>
-    <link href="https://yu.academic-gihara0655.com/bootstrap-5.3.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cook.academic-gihara0655.com/bootstrap-5.3.2-dist/js/bootstrap.min.js"></script>
+    <link href="https://cook.academic-gihara0655.com/bootstrap-5.3.2-dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
-    <link type="text/css" rel="stylesheet" href="{{ asset('index.css') }}"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('index.css') }}" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 </head>
-<body>
-  <div class="header fixed-top">
-    <div class="container-fluid p0">
-      <div class="row border">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light bg-color-2">
-            <!--<div class="col-lg-2">
-            </div>-->
-            <div class="col-2 d-flex">
-                <!--<a><img class="logo" src="{{asset('image/logo.png')}}"></a>-->
-                <button type="button"  style="border: none; background: transparent;" class="btn"><a href="{{ route('top') }}"><img class="logo" src="{{asset('image/logo.png')}}"></a></button>
-            </div>
-            <div class="col-lg-1">
-            </div>
-            <div class="col-lg-1">
-            <button type="button" class="btn"><a href="{{ route('top') }}" class="no-underline text-dark"><img src="{{ asset('image/cooking.png') }}" class="headerbotton"><br>通常モード</a></button>
-            </div>
-            <div class="col-lg-1">
-            <button type="button" class="btn"><a href="{{ route('home') }}" class="no-underline text-dark"><img src="{{ asset('image/home.png') }}" class="headerbotton"><br>ホーム</a></button>
-            </div>
-            <div class="col-lg-1">
-            <button type="button" class="btn"><a href="{{ route('post') }}" class="no-underline text-dark"><img src="{{ asset('image/post.png') }}" class="headerbotton"><br>投稿</a></button>
-            </div>
-            <div class="col-lg-1">
-            <button type="button" class="btn"><a href="{{ route('profile') }}" class="no-underline text-dark"><img src="{{ asset('image/profile.png') }}" class="headerbotton"><br>プロフィール</a></button>
-            </div>
-            <div class="col-lg-4">
-            </div>
-            <div class="col-1">
-                @guest
-                <a href="#modalProfile" data-bs-toggle="modal"><img src="{{ asset('image/user_icon.png') }}" class="user-icon" style="width: 48px; height: 48px;"/></a>
-                 @else
-                <a href="#modalProfile" data-bs-toggle="modal"><img src="{{ asset('') }}" class="user-icon" style="width: 48px; height: 48px;"/></a>
-                 @endguest
-            </div>
-        </nav>
-      </div>
-    </div>
-  </div>
-  <style>
 
+<body>
+    <div class="header fixed-top">
+        <div class="container-fluid p0">
+            <div class="row border">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light bg-color-2">
+                    <!--<div class="col-lg-2">
+            </div>-->
+                    <div class="col-2 d-flex">
+                        <!--<a><img class="logo" src="{{asset('image/logo.png')}}"></a>-->
+                        <button type="button" style="border: none; background: transparent;" class="btn"><a href="{{ route('top') }}"><img class="logo" src="{{asset('image/logo.png')}}"></a></button>
+                    </div>
+                    <div class="col-lg-1">
+                    </div>
+                    <div class="col-lg-1">
+                        <button type="button" class="btn"><a href="{{ route('top') }}" class="no-underline text-dark"><img src="{{ asset('image/cooking.png') }}" class="headerbotton"><br>通常モード</a></button>
+                    </div>
+                    <div class="col-lg-1">
+                        <button type="button" class="btn"><a href="{{ route('home') }}" class="no-underline text-dark"><img src="{{ asset('image/home.png') }}" class="headerbotton"><br>ホーム</a></button>
+                    </div>
+                    <div class="col-lg-1">
+                        <button type="button" class="btn"><a href="{{ route('post') }}" class="no-underline text-dark"><img src="{{ asset('image/post.png') }}" class="headerbotton"><br>投稿</a></button>
+                    </div>
+                    <div class="col-lg-1">
+                        <button type="button" class="btn"><a href="{{ route('profile') }}" class="no-underline text-dark"><img src="{{ asset('image/profile.png') }}" class="headerbotton"><br>プロフィール</a></button>
+                    </div>
+                    <div class="col-lg-4">
+                    </div>
+                    <div class="col-1">
+                        @guest
+                        <a href="#modalProfile" data-bs-toggle="modal"><img src="{{ asset('image/user_icon.png') }}" class="user-icon" style="width: 48px; height: 48px;" /></a>
+                        @else
+                        <a href="#modalProfile" data-bs-toggle="modal"><img src="{{ asset('') }}" class="user-icon" style="width: 48px; height: 48px;" /></a>
+                        @endguest
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <style>
         @media(max-width:750px) {
             .headerbotton {
 
@@ -69,4 +70,4 @@
     </style>
 </body>
 
-  @extends('layouts.modal')
+@extends('layouts.modal')
