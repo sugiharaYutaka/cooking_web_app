f4930cd96be0e423aa62bffb0f31e6ea7011c283
Murakawa Ryouya <kd1313614@st.kobedenshi.ac.jp>
2023-12-15 11:24:54 +0900

    k

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -98,12 +98,6 @@ class RegisterController extends Controller
             'password' => $request->password,
         ]);
 
-        Models\SnsProfile::create([
-            'email' => $request->email,
-            'history' => 0,
-            'comment' => '',
-        ]);
-
         return redirect()->route('top');
     }
 }
