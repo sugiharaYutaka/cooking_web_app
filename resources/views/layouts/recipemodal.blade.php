4d7b277e9af1d0157efa2503e5320a330176bf57
sugano taiki <kd1306256@st.kobedenshi.ac.jp>
2024-01-12 11:36:24 +0900

    タグ選択ヘッダー修正

--- resources/views/layouts/recipemodal.blade.php
+++ resources/views/layouts/recipemodal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header bg-color-2">
-                    <h5 class="modal-title" id="exampleModalLabel">工程選択</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">タグ選択</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
