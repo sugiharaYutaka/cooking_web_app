6ee800ce920b3e4ec467fcc6a898f04ab9169542
sugano taiki <kd1306256@st.kobedenshi.ac.jp>
2024-01-12 11:30:12 +0900

    タグ検索もーでる

--- resources/views/layouts/recipemodal.blade.php
+++ resources/views/layouts/recipemodal.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <title>Bootstrap Radio Buttons in PHP</title>
-</head>
 <body>
     <div class="modal fade" id="recipemodalProfile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -15,99 +7,57 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
-                <div class="modal-body bg-color-1">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="container">
-                                <div class="row justify-content-center">
-                                    <div class="col-md-10">
-                                        <div class="card-body">
-                                            <lanel for="image" class="form-label2 col-3" >工程名を選択</lanel>
-                                            <div class="container mt-3">
-                                                <div class="row">
-                                                    <form action="process_form.php" method="post">
+                    <div class="modal-body bg-color-1">
+                    <div class="">
+                    <h2>タグで検索</h2>
+                    <form action="{{ route('tags') }}" method="GET">
+                        <div class="form-group">
+                            <label>材料：</label><br>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag1" name="tags[]" value="魚">
+                                <label class="form-check-label" for="tag1">魚</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag2" name="tags[]" value="牛肉">
+                                <label class="form-check-label" for="tag2">牛肉</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag2" name="tags[]" value="豚肉">
+                                <label class="form-check-label" for="tag2">豚肉</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag3" name="tags[]" value="野菜">
+                                <label class="form-check-label" for="tag3">野菜</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag4" name="tags[]" value="卵">
+                                <label class="form-check-label" for="tag4">卵</label>
+                            </div>
 
-                                                            <div class="form-group">
-                                                                <div class="form-check">
-                                                                    <input class="form-check-input" type="radio" name="options" id="option1" value="option1">
-                                                                    <label class="form-check-label" for="option1">
-                                                                        下処理
-                                                                    </label>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option2" value="option2">
-                                                                <label class="form-check-label" for="option2">
-                                                                    焼く
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option4" value="option3">
-                                                                <label class="form-check-label" for="option3">
-                                                                    煮る
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option4" value="option4">
-                                                                <label class="form-check-label" for="option4">
-                                                                    揚げる
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option5" value="option5">
-                                                                <label class="form-check-label" for="option5">
-                                                                    茹でる
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option6" value="option6">
-                                                                <label class="form-check-label" for="option6">
-                                                                    炒める
-                                                                </label>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option7" value="option7">
-                                                                <label class="form-check-label" for="option7">
-                                                                    盛り付け
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="options" id="option8" value="option8">
-                                                                <label class="form-check-label" for="option8">
-                                                                    その他
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <textarea class="form-control col-12" id="userInput" name="text" style="height: 40px;"></textarea>
-                                                        <button type="submit" class="btn btn-primary mt-1">決定</button>
-                                                    </form>
-                                                    
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>  
+                            <label class="mt-2">調理法：</label><br>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag1" name="tags[]" value="焼き料理">
+                                <label class="form-check-label" for="tag1">焼き料理</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag2" name="tags[]" value="ゆで料理">
+                                <label class="form-check-label" for="tag2">ゆで料理</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag3" name="tags[]" value="煮込み料理">
+                                <label class="form-check-label" for="tag3">煮込み料理</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tag4" name="tags[]" value="揚げ料理">
+                                <label class="form-check-label" for="tag4">揚げ料理</label>
+                            </div>
+                            <!-- 他のタグを追加 -->
                         </div>
-                    </div>
+                        <button type="submit" class="btn btn-primary">検索</button>
+                    </form>
+                </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
-</html>
