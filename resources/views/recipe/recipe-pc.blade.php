879a853c79c750b68984e6c1c169a42d0e257e0a
takuworks123 <takuworks123@gmail.com>
2024-01-16 13:43:36 +0900

    [add] 献立一覧に「もっとみるボタン」追加

--- resources/views/recipe/recipe-pc.blade.php
+++ resources/views/recipe/recipe-pc.blade.php
@@ -10,7 +10,7 @@
             <div class="col-9 p-0">
                 <div class="container">
 
-                    <div class="row border">
+                    <div id="recipe-container" class="row border">
                         @foreach ($recipePost as $post)
                         <div class="col-4 my-2 border">
                             <p class="h5">{{ $post->title }}</p>
@@ -26,10 +26,30 @@
                         @endforeach
                     </div>
                 </div>
+                <button type="button" id="load-more" class="more-post btn btn-secondary">もっと見る</button>
             </div>
         </div>
     </div>
 
 </body>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        var page = 2; // 初期ページ
+
+        $('#load-more').on('click', function() {
+            $.ajax({
+                url: '/recipesMore',
+                type: 'GET',
+                data: { page: page },
+                success: function(response) {
+                    $('#recipe-container').append(response);
+                    page++;
+                }
+            });
+        });
+    });
+</script>
 
 </html>
