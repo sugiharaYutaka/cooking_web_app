efd076e2ae58f705c392ccfa8ae00d20eb3980b6
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-01-17 14:00:05 +0900

    [fix]

--- resources/views/recipe/recipe-phone.blade.php
+++ resources/views/recipe/recipe-phone.blade.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body style="margin-top: 8vh;">
+<body class="body-margin">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
