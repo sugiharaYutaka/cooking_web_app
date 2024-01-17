0404e3623e459dd289afdd811c17f6bad58650fe
takuworks123 <takuworks123@gmail.com>
2024-01-17 10:12:29 +0900

    [add] 献立一覧の「もっと見る」ボタンをスマホ画面に追加

--- resources/views/recipe/recipe-phone.blade.php
+++ resources/views/recipe/recipe-phone.blade.php
@@ -39,7 +39,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row border my-5">
+                    <div id="recipe-container" class="row border my-5">
                         @foreach ($recipePost as $post)
                         <div class="col-4 my-2 border">
                             <p class="h5">{{ $post->title }}</p>
@@ -56,11 +56,31 @@
                         @endforeach
                     </div>
                 </div>
+                <button type="button" id="load-more" class="more-post btn btn-secondary">もっと見る</button>
             </div>
         </div>
     </div>
 
 </body>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        var page = 2; // 初期ページ
+
+        $('#load-more').on('click', function() {
+            $.ajax({
+                url: '/recipesMore',
+                type: 'GET',
+                data: { page: page },
+                success: function(response) {
+                    $('#recipe-container').append(response);
+                    page++;
+                }
+            });
+        });
+    });
+</script>
 
 </html>
 @extends('layouts.recipemodal')
