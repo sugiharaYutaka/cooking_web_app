fb74f20b407fa55b52c10884e73483988bbff86e
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-01-18 11:57:08 +0900

    [style]

--- resources/views/recipe/recipe-pc.blade.php
+++ resources/views/recipe/recipe-pc.blade.php
@@ -16,7 +16,7 @@
                             <p class="h5">{{ $post->title }}</p>
 
                             <a href="{{ url('/recipe/onepost', $post->id) }}">
-                                <img src="{{ asset('/recipe/image/' . $post->dish_image_filename) }}" class="img-fluid rounded-2"
+                                <img src="{{ asset('/recipe/image/' . $post->dish_image_filename) }}" class="img-fluid rounded-1"
                                     style="width:100%; max-height: 200px; object-fit: cover; object-position: center;">
                             </a>
 
