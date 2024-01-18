af4388031127971ca602cd762094df041697d08e
takuworks123 <takuworks123@gmail.com>
2024-01-18 13:53:35 +0900

    [add] ブックマーク一覧の内容が２列になるように

--- resources/views/recipe/recipebookmark.blade.php
+++ resources/views/recipe/recipebookmark.blade.php
@@ -14,19 +14,19 @@
 <h1>ブックマーク</h1>
 
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-between">
         @foreach ($result as $post)
-        <div class="row border my-2 bg-color-1">
-            <div class="col-12 my-2">
+        <div class="row border col-md-6 my-2 bg-color-1">
+            <div class="my-2">
                 <span class='h5'>タイトル</span>
                 <span>{{ $post->title }}</span>
             </div>
-            <div class="col-12 my-2">
+            <div class="my-2">
                 <a href="{{ url('/recipe/onepost', $post->id) }}">
                     <img src="{{ asset('/recipe/image/' . $post->dish_image_filename) }}" class="" style="width:30%;">
                 </a>
             </div>
-            <div class="col-12 my-2">
+            <div class="my-2">
                 <span class='h5'>レシピの説明</span>
                 <span>{{ $post->description }}</span>
             </div>
